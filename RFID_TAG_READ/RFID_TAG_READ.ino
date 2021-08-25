2291dbe33192385f1d1edfa034122fb9d41756ef
BrijeshMakwana18 <bamakwana2000@gmail.com>
2021-08-25 22:17:09 +0530

    Project file added

---
+++ RFID_TAG_READ/RFID_TAG_READ.ino
@@ -0,0 +1,54 @@
+
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   
+ 
+void setup() 
+{
+  Serial.begin(9600);  
+  SPI.begin();      
+  mfrc522.PCD_Init();
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "F9 6D B5 99") 
+  {
+    Serial.println("TAG 1");
+    Serial.println();
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println("TAG 2");
+    delay(3000);
+  }
+} 
